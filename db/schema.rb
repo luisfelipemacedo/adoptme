d16b339bd18de226477bc356c1b2159abcc5e43a
William Rocha <wiilliamdroocha@gmail.com>
2023-08-28 18:08:27 -0300

    associacion and validates for pet

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_200032) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_204404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.date "birthday"
+    t.text "description"
+    t.string "category"
+    t.string "breed"
+    t.bigint "seller_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["seller_id"], name: "index_pets_on_seller_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_200032) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pets", "users", column: "seller_id"
 end

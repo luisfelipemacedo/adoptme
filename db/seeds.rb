e6673ac6e9ab96288c2c8b2ed06fa601f25a9d19
William Rocha <wiilliamdroocha@gmail.com>
2023-08-29 12:38:54 -0300

    almost done seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Pet.destroy_all
+
+seller_data = []
+20.times do
+  first = Faker::Name.first_name
+  last = Faker::Name.last_name
+  user = User.create!({
+    first_name: first,
+    last_name: last,
+    email: "#{first[0]}_#{last}@example.com".downcase,
+    passoword: "123123",
+    phone: Faker::PhoneNumber.phone_number
+  })
+  seller_data << user
+end
+
+random_pet_category = ["dog", "cat", "reptile", "bird", "rabbit", "rodent", "amphibian", "fish"]
+random_pet_breed = ["cur", "gatinho miado", "cachorro de rua", "jacaré do papo amarelo"]
+
+
+20.times do
+  pet_data = {
+    name: Faker::Name.first_name,
+    category: random_pet_category.sample,
+    date_of_birth: Faker::Date.between(from: 8.years.ago, to: Date.today),
+    description: Faker::Name.last_name,
+    breed: random_pet_breed.sample
+  }
+  pet = Pet.new(pet_data)
+  random_seller = seller_data.sample
+  pet.seller = random_seller
+  pet.save!
+
+end
+puts "seed completed"

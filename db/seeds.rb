9d92b6333c0525f8d4a9d7b1da413c7e5f3af730
Filipe Mariano <filipemarianoc@gmail.com>
2023-08-29 18:09:33 -0300

    fotos de pets prontas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 Pet.destroy_all
 User.destroy_all
 
@@ -15,13 +17,15 @@ seller_data = []
     birthday: Faker::Date.between(from: 40.years.ago, to: 18.years.ago)
   })
   seller_data << user
+  puts user.email
 end
 
 random_pet_category = %w[dog cat fish bird rodent amphibian reptile others]
 random_pet_breed = ["cur", "gatinho miado", "cachorro de rua", "jacaré do papo amarelo", "nemo","pernalonga"]
 
+pet_pics = ["https://www.itl.cat/pngfile/big/325-3250249_1584-golden-retriever-dog-puppy-wallpaper-beautiful-golden.jpg", "https://th.bing.com/th/id/R.786653caa036289ba5978f8a2838bddb?rik=Jace9ATfPow9Cg&riu=http%3a%2f%2fimages6.fanpop.com%2fimage%2fphotos%2f39300000%2fDog-dogs-39323649-1440-900.jpg&ehk=2j5hJZOGQYQL6IK9FydtWO%2fMjeCvMXSLLgWS2j9jERE%3d&risl=&pid=ImgRaw&r=0"]
 
-20.times do
+3.times do
   pet_data = {
     name: Faker::Name.first_name,
     category: random_pet_category.sample,
@@ -30,8 +34,10 @@ random_pet_breed = ["cur", "gatinho miado", "cachorro de rua", "jacaré do papo 
     breed: random_pet_breed.sample
   }
   pet = Pet.new(pet_data)
+  file = URI.open(pet_pics.sample)
   random_seller = seller_data.sample
   pet.seller = random_seller
+  pet.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
   pet.save!
 
 end

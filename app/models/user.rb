c6d2be1d209a1a509876907352db249d143441cb
William Douglas Rocha <127842211+WiilliamRocha@users.noreply.github.com>
2023-08-31 17:07:17 -0300

    Revert "Revert "Newhome""

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_one_attached :photo
   has_many :adoptions
   #validations
   validates :first_name, :last_name, :phone_number, :address, :birthday, presence: true

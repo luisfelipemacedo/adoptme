60d6c911e6d6bafc9a29e468b13affbb511571a5
Pedro Moretzsohn <p.moretzsohn24@gmail.com>
2023-08-31 13:06:04 -0300

    done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,11 +2,7 @@ class PetsController < ApplicationController
   before_action :set_pet, only: %i[show edit update destroy]
   def index
     @pets = Pet.all
-<<<<<<< HEAD
-    # @adoptions = Adoption.where(status: false)
-=======
     @pets = Pet.search_pets(params[:query]) if params[:query].present?
->>>>>>> master
   end
 
   def show
